96c60e62f17d10f933176c5e83d4ae3a6dad7a65
sflorezs1 <sflorezs1@eafit.edu.co>
2019-10-21 12:44:49 -0500

    update

--- New src/GUI.py
+++ New src/GUI.py
@@ -3,10 +3,16 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 import time
 import pandas as pd
-import turtle
 import pyC45reimplementation
 import Node
 from tkinter import messagebox
+import graphviz
+import os
+from PIL import Image, ImageTk
+from rsvg import *
+import cairo
+
+os.environ["PATH"] += os.pathsep + 'C:\\Program Files (x86)\\Graphviz2.38\\bin'
 
 
 class GUI(object):
@@ -15,6 +21,7 @@ class GUI(object):
 
     def __init__(self, width, height):
         windll.shcore.SetProcessDpiAwareness(1)
+        self.graph = graphviz.Digraph(comment="Decision Tree Model")
         self.window = Tk()
         self.width = width
         self.height = height
@@ -41,7 +48,9 @@ class GUI(object):
 
                 root: Node = pyC45reimplementation.load_model(filename)
                 self.tree = root
-                self.drawtree(root)
+                self.translate_to_DOT(root)
+                self.render_image()
+                print(self.graph.source)
                 pyC45reimplementation.print_tree(root)
                 pyC45reimplementation.print_tree(root)
             except FileNotFoundError:
@@ -67,7 +76,7 @@ class GUI(object):
                 filename = askopenfilename()
                 tree: Node = pyC45reimplementation.train(pd.read_csv(filename))
                 self.tree = tree
-                self.drawtree(tree)
+                self.translate_to_DOT(tree)
                 pyC45reimplementation.print_tree(tree)
             except FileNotFoundError:
                 answer = messagebox.askretrycancel("Question", "Do you want to try that again?")
@@ -90,48 +99,44 @@ class GUI(object):
         existent_button.pack()
         train_button = Button(frame_1, text="train new model", command=clicked_train)
         train_button.pack()
-
-        base_a = (0, 0)
-        base_b = (30, 20)
-        self.draw_line(base_a[0], base_b[0], base_a[1], base_b[1])
         self.window.mainloop()
 
-    def draw_line(self, x1, y1, x2, y2):
-        self.canvas.create_line(x1, y1, x2, y2, fill="black")
-        self.canvas.pack()
+    def translate_to_DOT(self, tree: Node, node_name: str = "a"):
+        if tree.results is not None:
+            label: str = ""
+            label += tree.results
+            self.graph.node(node_name, label, shape=("square" if label.__contains__("no") else "diamond"),
+                            color=("crimson" if label.__contains__("no") else "dodgerblue2"),
+                            style="filled", fillcolor=("red" if label.__contains__("no") else "cyan"))
+        else:
+            this_info: str = tree.attribute + " : " + str(tree.value) + "?"
+            self.graph.node(node_name, this_info, shape="box", color="blueviolet", style="filled", fillcolor="gray")
+            node_name_left: str = node_name + "l"
+            self.translate_to_DOT(tree.left_child, node_name_left)
+            self.graph.edge(node_name, node_name_left, label="True")
+            node_name_right: str = node_name + "r"
+            self.translate_to_DOT(tree.right_child, node_name_right)
+            self.graph.edge(node_name, node_name_right, label="False")
 
-    def draw_tree(self, root: Node):
-        return 0
+    def render_image(self):
+        self.graph.format = "svg"
+        self.graph.render()
 
-    def drawtree(self, root):
-        def height(root):
-            return 1 + max(height(root.left_child), height(root.right_child)) if root else -1
-
-        def jumpto(x, y):
-            t.penup()
-            t.goto(x, y)
-            t.pendown()
-
-        def draw(node, x, y, dx):
-            if node:
-                t.goto(x, y)
-                jumpto(x, y - 20)
-                t.write(node.value, align='center', font=('Arial', 12, 'normal'))
-                draw(node.left_child, x - dx, y - 60, dx / 2)
-                jumpto(x, y - 20)
-                draw(node.right_child, x + dx, y - 60, dx / 2)
-
-        t = turtle.Turtle()
-        t.speed(0)
-        turtle.delay(0)
-        h = height(root)
-        jumpto(0, 30 * h)
-        draw(root, 0, 30 * h, 40 * h)
-        t.hideturtle()
-        turtle.mainloop()
+    def svgPhotoImage(self, file_path_name):
+        """Returns a ImageTk.PhotoImage object represeting the svg file"""
+        # Based on pygame.org/wiki/CairoPygame and http://bit.ly/1hnpYZY
+        svg = cairosvg.
+        width, height = svg.get_dimension_data()[:2]
+        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, int(width), int(height))
+        context = cairo.Context(surface)
+        # context.set_antialias(cairo.ANTIALIAS_SUBPIXEL)
+        svg.render_cairo(context)
+        tk_image = ImageTk.PhotoImage('RGBA')
+        image = Image.frombuffer('RGBA', (width, height), surface.get_data(), 'raw', 'BGRA', 0, 1)
+        tk_image.paste(image)
+        return tk_image
 
 
 if __name__ == '__main__':
     gui = GUI(800, 600)
     gui.gui_init()
-
