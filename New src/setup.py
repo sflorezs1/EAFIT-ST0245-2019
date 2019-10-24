498a14adb707533e189ae340509277d3f9a84b75
sflorezs1 <sflorezs1@eafit.edu.co>
2019-10-24 16:24:06 -0500

    Updated: Run main at GUI.py

---
+++ New src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Practice Data',
+    version='00001',
+    packages=['C45GUI'],
+    url='',
+    license='NONE',
+    author='sflorezs1',
+    author_email='sflorezs1@eafit.edu.co',
+    description=''
+)
